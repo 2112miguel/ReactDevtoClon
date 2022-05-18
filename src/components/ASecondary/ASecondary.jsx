a80059227cb7b5f4b67f0cc86fbd0180d67bb26c
dragonfire124 <whitescorpion26@gmail.com>
2022-05-18 17:31:31 -0500

    2 files modified

--- src/components/ASecondary/ASecondary.jsx
+++ src/components/ASecondary/ASecondary.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './ASecondary.scss';
 
-export const ASecondary = () => {
+export const ASecondary = ({ text }) => {
   return (
     <div className="ASecondary">
       <div className="ASecondary-container">
-        <div className="ASecondary-content">Lorem</div>
+        Adding headers to the response in an angular aplication
       </div>
     </div>
   );
