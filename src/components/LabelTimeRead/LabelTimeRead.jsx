7095440358be8d905c85ea0c79408fbe0704590e
Arnold Esquivel <arnold_526@outlook.com>
2022-05-18 10:11:30 -0500

    Se agrega estilado a Pages/Post

--- src/components/LabelTimeRead/LabelTimeRead.jsx
+++ src/components/LabelTimeRead/LabelTimeRead.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const LabelTimeRead = () => {
   return (
-    <div className="text-muted m-1 col-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+    <div className="text-muted">
       1 min Read
     </div>
   );
