5b164b7445ba2bc71635426a5986fa52830311a1
dragonfire124 <whitescorpion26@gmail.com>
2022-05-18 02:05:13 -0500

    2 files modified

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,6 +11,7 @@ import { ButtonGeneral } from "../components/ButtonGeneral/ButtonGeneral";
 import { CardPost } from "../components/CardPost/CardPost";
 import { AppContext } from "../Context/AppContext";
 import { CardMain } from "../components/CardMain/CardMain";
+import {ASecondary}  from '../components/ASecondary/ASecondary'
 
 const URL = "https://devtoclon.herokuapp.com/posts";
 
@@ -83,7 +84,8 @@ export const Home = () => {
             </div>
           </div>
           <div className="col-3">
-            <CardMain></CardMain>
+            <CardMain> <ASecondary/></CardMain>
+
           </div>
         </article>
       </section>
